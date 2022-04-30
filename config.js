0f726dfb2cc623ed1ce43a33acfe219fd4d23890
sunilkmathew <sunilkmathew35@gmail.com>
2022-04-30 20:26:08 +0530

    Added the project in firebase database

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBTSkQnK2SQdmc5yhMGqti-NQbAa6fT0j0",
+    authDomain: "storytelling-app-ce2ec.firebaseapp.com",
+    projectId: "storytelling-app-ce2ec",
+    storageBucket: "storytelling-app-ce2ec.appspot.com",
+    messagingSenderId: "974168036926",
+    appId: "1:974168036926:web:83b79613fc3c63e4cda36f"
+};
